9d88736d8e4eefebd289dff5c7816d73b11903d6
CharlesGdn <charles.gandon@yahoo.com>
2020-04-20 15:51:50 +0200

    added print count value

--- src/count/__init__.py
+++ src/count/__init__.py
@@ -3,21 +3,24 @@ import numpy as np
 
 img_rgb = cv2.imread('DJI_0554_0.png')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('template.png',0)
+template = cv2.imread('template2.png',0)
 w, h = template.shape[::-1]
 
 res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-threshold = 0.8 
+threshold = 0.8
 loc = np.where( res >= threshold)
 
 f = set()
+count = 0
 
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
     sensitivity = 100
     f.add((round(pt[0]/sensitivity), round(pt[1]/sensitivity)))
+    count = count + 1
 
 cv2.imwrite('res.png',img_rgb)
 
 found_count = len(f)
+print(found_count)
