fec6b40926f8caa12ff8ea8dbb9c269acf82cdb1
JulienBedel <bedel.ju@gmail.com>
2020-05-11 18:46:21 +0200

    feat(global): Add minor bug fixes
    - Fix image write by counter
    - Fix CD pipeline

--- src/count/counter.py
+++ src/count/counter.py
@@ -1,42 +1,35 @@
 import cv2
+import imutils
 import numpy as np
-
-import pathlib
 
 known_images = ['crowd.png']
 known_images_path = './src/count/known_images/'
 
-def count(image_path):
 
+def count(image_path):
     count = 0
 
-    image = open(image_path,"rb").read()
+    image = open(image_path, "rb").read()
     image_found = None
 
     for known_image in known_images:
-        if image == open(known_images_path + known_image,"rb").read():
+        if image == open(known_images_path + known_image, "rb").read():
             image_found = known_image
             break
-    
-    if(not image_found):
-        return -1 
-    
-    img = cv2.imread(image_path)
-    imgYCC = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
-    gray = cv2.cvtColor(imgYCC, cv2.COLOR_BGR2GRAY)
 
-    print(known_images_path + 'template_{}'.format(image_found))
-    template = cv2.imread((known_images_path + 'template_{}').format(image_found), 0)
+    if (not image_found):
+        return -1
 
-    w, h = template.shape[::-1]
+    img_rgb = cv2.imread(image_path)
+    imgYCC = cv2.cvtColor(img_rgb, cv2.COLOR_YCrCb2RGB)
+    img_gray = cv2.cvtColor(imgYCC, cv2.COLOR_BGR2GRAY)
 
-    res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+    lower = np.array([178])
+    upper = np.array([202])
+    shapeMask = cv2.inRange(img_gray, lower, upper)
 
-    threshold = 0.77
+    cnts = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
 
-    loc = np.where(res >= threshold)
-
-    for pt in zip(*loc[::-1]):
-        count = count + 1
-
-    return count
+    return len(cnts)
